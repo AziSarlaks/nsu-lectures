a575fa801de60dccdcc5e1cf6a95d95204ac7ade
pooshok2007@mail.ru <pooshok2007@mail.ru>
2025-10-04 15:57:25 +0700

    fffff

--- imperativeprogramming/tasksfour/five.c
+++ imperativeprogramming/tasksfour/five.c
@@ -1,74 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-char *split(char *Str, char *separators) {
-    static char res[1000001] = {0};
-    static char res2[1000001] = {0};
-    int len = 0;
-    int alpha = 0;
-    while (*Str) {
-        if (!strchr(separators, *Str)) {
-            char temp[2] = {*Str, '\0'};
-            strcat(res, temp);
-            len++;
-            if ('A' <= *Str && *Str <= 'Z') {
-                alpha++;
-            }
-            Str++;
-        } else if (strchr(separators, *Str) && len == 0) {
-            Str++;
-        } else if (strchr(separators, *Str)){
-            char l = len + '0';
-            char a = alpha + '0';
-            char temp[4] = {a, '/', l, '\0'};
-            strcat(res2, temp);
-            strcat(res2, res);
-            res[0] = '\0';
-            len = 0;
-            alpha = 0;
-            Str++;
-        }
-        if (len > 0 && *Str == '\0') {
-            char l = len + '0';
-            char a = alpha + '0';
-            strcat(res2, (char[]){a, '/', l, '\0'});
-            strcat(res2, res);
-        }
-    }
-    return res2;
+int issep(char ch) {
+    if (ch == '.' || ch == ',' || ch == ':' || ch == ';') return 1;
+    return 0;
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    char sep[] = {'.', ',', ':', ';', '\0'};
     char in[1000001];
     scanf("%s", in);
+    int l = 0;
+    int a = 0;
+    int f = 0;
+    char s[1000001] = {0};
     fclose(stdin);
-    char *res = split(in, sep);
-    if (res[0] != '\0') {
-
-        printf("%c%c%c ", res[0], res[1], res[2]);
-        int f = 1;
-        for (int i = 3; i < 10000000; i++) {
-            if ('0' <= res[i] && res[i] <= '9' && f == 1) {
-                f = 0;
-                printf("\n");
-                printf("%c", res[i]);
-            } else if (f == 0 && '/' == res[i]) {
-                printf("%c", res[i]);
-                f = 2;
-            } else if ('0' <= res[i] && res[i] <= '9' && f == 2) {
-                printf("%c", res[i]);
-                printf(" ");
-                f = 1;
-            } else if (res[i] == '\0') {
-                break;
-            } else {
-                printf("%c", res[i]);
-            }
+    for (int i = 0; i<10000000; i++) {
+        if (in[i] == '\0' && l != 0) {
+            printf("%d/%d %s\n", a, l, s);
+            return 0;
+        } else if (in[i] == '\0' && l == 0) {
+            return 0;
         }
-        fclose(stdout);
+        if (issep(in[i]) && l != 0) {
+            printf("%d/%d %s\n", a, l, s);
+            a = 0;
+            l = 0;
+            s[0] = '\0';
+        } else if (!issep(in[i])) {
+            strcat(s, (char[]){in[i], '\0'});
+            if ('A' <= in[i] && in[i] <= 'Z') a++;
+            l++;
+        }
     }
     return 0;
 }
---
+++ imperativeprogramming/tasksfour/six.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int readTime(char *iStr, int *oHours, int *oMinutes, int *oSeconds) {
+    while (*iStr)
+}
